4bb64f8a2434eb84c729f989bdd1d530d28d77e3
mrcoder9 <aarnavakeshpillai@yahoo.com>
2021-01-01 13:53:35 +0530

    adding modified files

--- Pig.js
+++ Pig.js
@@ -2,6 +2,23 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    pop();
+    this.Visiblity = 255
   }
 
+  display()
+  {
+    console.log(this.body.speed);
+    if(this.body.speed<3)
+    {
+        super.display();
+    }
+     else
+     {
+        World.remove(world,this.body);
+        push();
+        this.Visiblity = this.Visiblity-5
+        tint(255,this.Visiblty)
+     }
+  }
 };
